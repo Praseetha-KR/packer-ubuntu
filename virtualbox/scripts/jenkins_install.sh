6768e25a63dea7edd5de34f2a92c9e0ff268ed1d
Konstantin Kozulin <juvescorp@gmail.com>
2020-11-18 23:19:07 +0300

    Add script to install Java and Jenkins

---
+++ virtualbox/scripts/jenkins_install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
+    /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install -y openjdk-8-jdk 
+sudo apt-get install -y jenkins
